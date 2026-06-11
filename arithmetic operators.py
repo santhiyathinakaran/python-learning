a8f879890a31ba6a9b5736fd50f6e825234aef8a
santhiyathinakaran <santhiyathinakaran28@gmail.com>
2026-06-11 21:30:50 +0530

    Add files via upload

---
+++ arithmetic operators.py
@@ -0,0 +1,20 @@
+miniutes=int(input("enter the mimiutes:"))
+seconds=miniutes*60
+print("seconds",seconds)
+print("---------------------------------")
+meter=int(input("enter the meters:"))
+kg=meter*1000
+print("kilo meter",kg)
+print("-----------------------------------")
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+print("thrid angle of triangle is:",a+b%2)
+print("-------------------------------------")
+num=int(input("enter a number:"))
+lost_digit=num%10
+print("lost digita number:",lost_digit)
+print("---------------------------------")
+second_place_num=num%100
+print("second place number:",second_place_num)
+
+
