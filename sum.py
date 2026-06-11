22813b97ddda48e631374f25c53dfd9ca9efdde7
santhiyathinakaran <santhiyathinakaran28@gmail.com>
2026-06-11 21:26:48 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,9 @@
+b=int(input("enter the base:"))
+h=int(input("enter the heigh:"))
+print(1/2*b*h)
+print("-------------------")
+pi=3.14
+r=int(input("enter the value:"))
+area=2*pi*r
+print("area of the circle is:",area)
+
