a848f5ea4837c70d0dcd3cdfff6d0b44bac9133d
santhiyathinakaran <santhiyathinakaran28@gmail.com>
2026-06-17 16:27:02 +0530

    Add files via upload

---
+++ while task level1.py
@@ -0,0 +1,57 @@
+i=0
+while i<=9:
+    i=i+1
+    print(i)
+i=1
+sum=0
+i=11
+while i>=2:
+    i=i-1
+    print(i)
+while i<=10:
+    sum=sum+2
+    i=i+1
+    print(sum)
+i=-1
+while i<=17:
+    i=i+2
+    print(i)
+i=1
+sum=0
+while i<=50:
+    sum=sum+i
+    i=i+1
+print(sum)
+    
+i=1
+sum=1
+num=int(input("enter the num:"))
+while i<=num:
+    mark=int(input("Enter the number:")
+    sum=i*mark
+    i=i+1
+print(sum)
+i=1
+sum=1
+while i<=11:
+    sum=sum*i*i
+    i=i+2
+print(sum)
+i=0
+while i<=100:
+    i=i+1
+    print(i)
+    print()
+i=1
+sum=0
+while i<=25:
+    sum=sum+4
+    i=i+1
+    print(sum)
+i=2
+sum=0
+while i<=100:
+    sum=sum+i
+    i=i+2
+print(sum)
+3
