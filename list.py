13c9d7b53c6a62854d3e2fd757f5088156932d50
Santhiya P <santhiyathinakaran28@gmail.com>
2026-06-23 15:42:16 +0530

    Add files via upload

--- list.py
+++ list.py
@@ -1,13 +1,21 @@
-a=[1,2,3,4,5]
-print(a)
-a.append("san")
-a.append(True)
-a.append(1)
-print(a)
-a.insert(0,6)
-print(a)
-a.pop(0)
-print(a)
-b=[7,8,9]
-a.extend(b)
-print(a)
+list=["san",34,True,34.0,'apple',4,5,60.7,'pro']
+for i in list:
+    print(type(i))
+list=[1,2,3,4,5,6,7,8,9,10]
+for i in list:
+    if i%2!=0:
+        print(i)
+list=['s','a','n','t','h','i','y','a','t','h','i','n','a','k','a','r','a','n']
+if len(list)<6:
+        print(list)
+else:
+        print("not mached")
+list=['santhiya','thinakaran','vijaya','robin','cathrin']
+for i in list:
+    print(i.title())
+lis=['santhiya']
+for i in lis:
+    print(i)
+    for j in i:
+        print(j)
+    print("--------------")
