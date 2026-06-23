4d94f65ce40b5bef95cba24d75e863b0e4818bca
Santhiya P <santhiyathinakaran28@gmail.com>
2026-06-23 15:42:46 +0530

    Add files via upload

---
+++ list tasks.py
@@ -0,0 +1,37 @@
+list=[1,2,3,4,5]
+for i in list:
+    print(i)
+name=['santhiya','thinakaran','vijaya','robin','cathrin']
+for i in name:
+    print(i)
+list=[10,20,30,40]
+count=0
+for i in list:
+    count+=1
+print(count)
+num=[10,20,30,40,50]
+print(max(num))
+print(min(num))
+print(sum(num))
+num=[1,2,3,4,5,6,7,8,9,10,12]
+count=0
+for i in num:
+    if i%2==0:
+        count+=1
+print(count)
+num=[1,2,3,4,5,6,7,8,9,10,12]
+count=0
+for i in num:
+    if i%2!=0:
+        count+=1
+print(count)
+num=[1,-2,3,-4,5,6,7,-8,9,-10,12]
+for i in num:
+    if i>0:
+        print(i)
+num=[1,-2,3,-4,5,6,7,-8,9,-10,12]
+for i in num:
+    if i<0:
+        print(i)
+   
+
