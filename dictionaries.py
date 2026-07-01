88a24e0e6b897dd352adf639f1188268507f6e67
Santhiya P <santhiyathinakaran28@gmail.com>
2026-07-01 15:11:35 +0530

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+student={'name':'santhiya','age':10,'course':'python','address':{'dno':123,'city':'tirunelveli','street':'west street','pin':627152},'city':'palaiyankottai','num':10}
+x=student['name']
+print(x)
+y=student['address']
+print(y)
+z=student['city']
+print(z)
+a=student['address']['city']
+print(a)
+b=student['num']
+print(b)
